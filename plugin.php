4b3f3ac9d80efcffe171e68e66b5fc2b90cc7fe1
SebastianZiegler <zieglers@fh-nordhausen.de>
2019-03-11 12:02:35 +0100

    first commit

---
+++ plugin.php
@@ -0,0 +1,19 @@
+<?php
+ 
+// alphanumerical ID of the plugin; never change this
+$id = "xbbb";
+ 
+// code version; must be changed for all code changes
+$version = "1.0.9";
+ 
+// ilias min and max version; must always reflect the versions that should
+// run with the plugin
+$ilias_min_version = "4.0.0";
+$ilias_max_version = "5.2.999";
+ 
+// optional, but useful: Add one or more responsible persons and a contact email
+$responsible = "Andrés Ederra, Sebastian Ziegler";
+//$responsible_mail = "private";
+
+//$supports_export = true;
+?>
